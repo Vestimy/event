190482358323675f7322e6c053578abf2e840b51
Vestimy <vestimyandrey@gmail.com>
2021-03-05 23:11:54 +0300

    update

--- event/__init__.py
+++ event/__init__.py
@@ -238,6 +238,7 @@ def admin_required(func):
 
     return decorated_view
 
+
 def admin_company(func):
     """
     Modified login_required decorator to restrict access to admin group.
@@ -253,6 +254,16 @@ def admin_company(func):
 
     return decorated_view
 
+
+def decorated_login(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_anonymous:
+            if not 'admin' in current_user.roles:  # zero means admin, one and up are other groups
+                flash("You don't have permission to access this resource.", "warning")
+                return redirect(url_for("main.index"))
+        return func(*args, **kwargs)
+    return decorated_view
 
 def generate_id():
     return str(uuid.uuid4())
