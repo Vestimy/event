5a174d904720e12d18bb7fc367157724d2478efa
Vestimy <vestimyandrey@gmail.com>
2021-03-14 23:06:57 +0300

    update

--- event/views/security/views.py
+++ event/views/security/views.py
@@ -14,7 +14,7 @@ security = Blueprint('security', __name__)
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('security.login'))
 
 
 @security.route('/login', methods=['GET', 'POST'])
